ea862a8ba30bab74e42695dfbfef9ff7e58db03f
ProxxyX <shresthaprajjwol4@example.com>
2024-04-03 17:53:38 +0545

    fixed errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 
   async function fetchData() {
     try {
-      const res = await axios.get("http://localhost:3000/getdata")
+      const res = await axios.get("https://portfolio-mern-59ki/getdata")
       dispatch(getData(res.data))
       dispatch(reloadData(false))
     } catch (e) {
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dbConnection()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173", "*"]
+    origin: ["http://localhost:5173", "https://portfolio-mern-59ki"]
 }))
 
 app.use("/", portfolioRoutes)
