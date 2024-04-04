7c6f9c7529b9bc1743357447c086a85492457fe5
Prajjwol | Shrestha <132867834+prajjwolcodes@users.noreply.github.com>
2024-04-04 09:23:54 +0545

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+  server: {
+    port: 5173,
+    proxy: {
+      "/": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
 })
