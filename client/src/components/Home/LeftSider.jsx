aa074b6b6568ce0c31700fe6b9be028fe78dfa15
Prajjwol | Shrestha <132867834+prajjwolcodes@users.noreply.github.com>
2024-04-04 10:16:13 +0545

    Added admin icon

--- client/src/components/Home/LeftSider.jsx
+++ client/src/components/Home/LeftSider.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LeftSider = () => {
     return (
         <div className='fixed left-14 bottom-3 sm:static '>
             <div className="flex flex-col sm:flex-row sm:justify-center gap-3 items-center text-gray-600">
+                <Link to="/admin" target='_blank'><i class="ri-admin-line text-2xl"></i></Link>
                 <a href="https://www.facebook.com/profile.php?id=100011013391464" target='_blank'><i className="ri-facebook-circle-line text-2xl"></i></a>
                 <a href="https://www.instagram.com/prajzwolslimsulek/" target='_blank'><i className="ri-instagram-line text-2xl"></i></a>
                 <a href="https://github.com/prajjwolcodes" target='_blank'><i className="ri-github-line text-2xl"></i></a>
