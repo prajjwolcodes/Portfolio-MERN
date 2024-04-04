7df5045cc1250b3c0f107387ef997b1adfd742c9
Prajjwol | Shrestha <132867834+prajjwolcodes@users.noreply.github.com>
2024-04-04 10:11:44 +0545

    Update Login.jsx

--- client/src/components/Admin/Login/Login.jsx
+++ client/src/components/Admin/Login/Login.jsx
@@ -71,7 +71,7 @@ const Login = () => {
                             <div>
                                 <button
                                     type="submit"
-                                    className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    className="flex w-full justify-center rounded-md bg-[#6A64F1] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     Login
                                 </button>
